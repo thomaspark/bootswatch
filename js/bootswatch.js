5ebb528b56c3a677c323c364c1751239ec6aa657
Thomas Park <thomas@thomaspark.me>
2012-02-18 16:11:03 -0500

    append swatch menu with js

---
+++ js/bootswatch.js
@@ -0,0 +1,13 @@
+$('.navbar .nav').first().append('<li class="dropdown"> \
+	         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Preview <b class="caret"></b></a> \
+	         <ul class="dropdown-menu"> \
+	           <li><a href="../default">Default</a></li> \
+	           <li class="divider"></li> \
+	           <li><a href="../cerulean">Cerulean</a></li> \
+	           <li><a href="../cyborg">Cyborg</a></li> \
+	           <li><a href="../journal">Journal</a></li> \
+	           <li><a href="../simplex">Simplex</a></li> \
+	           <li><a href="../spacelab">Spacelab</a></li> \
+	           <li><a href="../united">United</a></li> \
+	         </ul> \
+	       </li>');
