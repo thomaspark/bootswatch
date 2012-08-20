c5aebfa780647d4d8c59f474aa945b9076d66ee5
Thomas Park <thomas@thomaspark.me>
2012-08-20 18:34:40 -0400

    index: add sponsor

---
+++ js/bsa.js
@@ -0,0 +1,7 @@
+(function(){
+  var bsa = document.createElement('script');
+     bsa.type = 'text/javascript';
+     bsa.async = true;
+     bsa.src = 'http://s3.buysellads.com/ac/bsa.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
