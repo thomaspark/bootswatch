4665a9218661e7160cd17c27cd620aafd2390564
Thomas Park <thomas@thomaspark.me>
2012-08-22 10:35:15 -0400

    index: added recent updates

--- js/bootswatch.js
+++ js/bootswatch.js
@@ -8,3 +8,13 @@ $('#main-menu').load('/js/snippets/main-menu.html', function(){
 		'placement': 'bottom'
 	});
 });
+
+function parseRSS(url, callback) {
+  $.ajax({
+    url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+    dataType: 'json',
+    success: function(data) {
+      callback(data.responseData.feed);
+    }
+  });
+}
