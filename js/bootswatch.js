e6092c3fe985fb326ad3625274bb021ce61bf80c
Thomas Park <thomas@thomaspark.me>
2012-11-16 11:55:47 -0500

    index: no longer using snippets

--- js/bootswatch.js
+++ js/bootswatch.js
@@ -1,12 +1,5 @@
-$('#main-menu').load('/js/snippets/main-menu.html', function(){
-	if($('body').hasClass('preview')){
-		$('#main-menu-left').append('<li class="dropdown" id="preview-menu"></li>');
-		$('#preview-menu').load('/js/snippets/preview-menu.html');
-	}
-
-	$('a[rel=tooltip]').tooltip({
-		'placement': 'bottom'
-	});
+$('a[rel=tooltip]').tooltip({
+	'placement': 'bottom'
 });
 
 function parseRSS(url, callback) {
