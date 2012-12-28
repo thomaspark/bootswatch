6176f1b12d9099e891dd8192cedcbd1e1b6cca6a
Thomas Park <thomas@thomaspark.me>
2012-12-27 23:07:25 -0500

    clean up javascript code

--- js/application.js
+++ js/application.js
@@ -1,29 +1,29 @@
-!function ($) {
+(function ($) {
 
-  $(function(){
+	$(function(){
 
-    // fix sub nav on scroll
-    var $win = $(window)
-      , $nav = $('.subnav')
-	  , navHeight = $('.navbar').first().height()
-      , navTop = $('.subnav').length && $('.subnav').offset().top - navHeight
-      , isFixed = 0
+		// fix sub nav on scroll
+		var $win = $(window),
+				$nav = $('.subnav'),
+				navHeight = $('.navbar').first().height(),
+				navTop = $('.subnav').length && $('.subnav').offset().top - navHeight,
+				isFixed = 0;
 
-    processScroll()
+		processScroll();
 
-    $win.on('scroll', processScroll)
+		$win.on('scroll', processScroll);
 
-    function processScroll() {
-      var i, scrollTop = $win.scrollTop()
-      if (scrollTop >= navTop && !isFixed) {
-        isFixed = 1
-        $nav.addClass('subnav-fixed')
-      } else if (scrollTop <= navTop && isFixed) {
-        isFixed = 0
-        $nav.removeClass('subnav-fixed')
-      }
-    }
+		function processScroll() {
+			var i, scrollTop = $win.scrollTop();
+			if (scrollTop >= navTop && !isFixed) {
+				isFixed = 1;
+				$nav.addClass('subnav-fixed');
+			} else if (scrollTop <= navTop && isFixed) {
+				isFixed = 0;
+				$nav.removeClass('subnav-fixed');
+			}
+		}
 
-  })
+	});
 
-}(window.jQuery)
+})(window.jQuery);
