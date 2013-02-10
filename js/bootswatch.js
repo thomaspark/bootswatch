930114be5b671cf8aab0914b146a164fa635d80a
Thomas Park <thomas@thomaspark.me>
2013-02-09 22:36:58 -0500

    moved parseRSS function to index

--- js/bootswatch.js
+++ js/bootswatch.js
@@ -4,18 +4,6 @@ $('a[rel=tooltip]').tooltip({
 	'placement': 'bottom'
 });
 
-// recent news
-
-function parseRSS(url, callback) {
-  $.ajax({
-    url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
-    dataType: 'json',
-    success: function(data) {
-      callback(data.responseData.feed);
-    }
-  });
-}
-
 // smooth scroll
 
 $(document).ready(function() {
