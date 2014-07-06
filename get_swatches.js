b4a50cb2c0a4041aa32b57bb3bf1d05bf964307d
Nicolas McCurdy <thenickperson@gmail.com>
2014-07-06 05:31:14 -0400

    Use Grunt's file matchers instead of removeBlackListed()

--- get_swatches.js
+++ get_swatches.js
@@ -1,28 +1,20 @@
 var grunt = require('grunt');
 
-// Returns a copy of the given Array with all elements of the blacklist Array
-// removed.
-function removeBlacklisted(array, blacklist) {
-  return array.filter(function (item) {
-    return blacklist.indexOf(item) === -1;
-  });
-}
-
 // Returns an Array of the names of all Bootswatch themes in the current
 // directory.
 module.exports = function () {
-  var dirs = grunt.file.expand({ filter: 'isDirectory' }, '*');
-
-  return removeBlacklisted(dirs, [
-    "2",
-    "api",
-    "assets",
-    "bower_components",
-    "default",
-    "fonts",
-    "global",
-    "help",
-    "node_modules",
-    "tests"
-  ]);
+  return grunt.file.expand(
+    { filter: 'isDirectory' },
+    "*",
+    "!2",
+    "!api",
+    "!assets",
+    "!bower_components",
+    "!default",
+    "!fonts",
+    "!global",
+    "!help",
+    "!node_modules",
+    "!tests"
+  );
 };
