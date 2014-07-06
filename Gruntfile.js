d9d8813b8f29644b10823b11f62ad16cd2f864e6
Nicolas McCurdy <thenickperson@gmail.com>
2014-07-06 05:44:46 -0400

    Generate the config for the swatch task programmatically

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
+  var swatchConfig = swatches.reduce(function (memo, swatch) {
+    memo[swatch] = {};
+    return memo;
+  }, {});
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -16,12 +21,7 @@ module.exports = function (grunt) {
             ' * Licensed under <%= pkg.license %>\n' +
             ' * Based on Bootstrap\n' +
             '*/\n',
-    swatch: {
-      amelia:{}, cerulean:{}, cosmo:{}, cyborg:{}, darkly:{},
-      flatly:{}, journal:{}, lumen:{}, readable:{}, simplex:{},
-      slate:{}, spacelab:{}, superhero:{}, united:{}, yeti:{},
-      custom:{}
-    },
+    swatch: swatchConfig,
     clean: {
       build: {
         src: ['*/build.less', '!global/build.less']
