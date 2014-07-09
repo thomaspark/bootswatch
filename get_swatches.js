f3e3ca1ad23bfd0d439e6423c21daf61bde53b93
Nicolas McCurdy <thenickperson@gmail.com>
2014-07-09 18:55:35 -0400

    Don't rely on a blacklist of directories for getSwatches()
    Instead of holding a list of excluded directories that would have to
    be manually updated, getSwatches() now looks for directories
    containing both variables.less and bootswatch.less.

--- get_swatches.js
+++ get_swatches.js
@@ -1,20 +1,17 @@
 var grunt = require('grunt');
+var fs = require ('fs');
 
 // Returns an Array of the names of all Bootswatch themes in the current
 // directory.
 module.exports = function () {
   return grunt.file.expand(
-    { filter: 'isDirectory' },
-    '*',
-    '!2',
-    '!api',
-    '!assets',
-    '!bower_components',
-    '!default',
-    '!fonts',
-    '!global',
-    '!help',
-    '!node_modules',
-    '!tests'
+    {
+      filter: function (src) {
+        return fs.statSync(src).isDirectory() &&
+               grunt.file.exists(src, 'bootswatch.less') &&
+               grunt.file.exists(src, 'variables.less');
+      }
+    },
+    '*'
   );
 };
