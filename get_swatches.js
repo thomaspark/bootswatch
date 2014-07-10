4d97dad49b2cbef14a1c380fc78441ae8aa52dd2
Nicolas McCurdy <thenickperson@gmail.com>
2014-07-09 20:45:09 -0400

    Refactor the file expand filter into a named function

--- get_swatches.js
+++ get_swatches.js
@@ -1,17 +1,15 @@
 var grunt = require('grunt');
 var fs = require ('fs');
 
+// Returns true if the given path (String) is a swatch directory.
+function isSwatchDirectory(src) {
+  return fs.statSync(src).isDirectory() &&
+         grunt.file.exists(src, 'bootswatch.less') &&
+         grunt.file.exists(src, 'variables.less');
+}
+
 // Returns an Array of the names of all Bootswatch themes in the current
 // directory.
 module.exports = function () {
-  return grunt.file.expand(
-    {
-      filter: function (src) {
-        return fs.statSync(src).isDirectory() &&
-               grunt.file.exists(src, 'bootswatch.less') &&
-               grunt.file.exists(src, 'variables.less');
-      }
-    },
-    '*'
-  );
+  return grunt.file.expand({ filter: isSwatchDirectory }, '*');
 };
