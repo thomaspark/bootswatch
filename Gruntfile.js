101ffa49e871a1be2724780baa4512c80aa18069
Thomas Park <thomas@thomaspark.co>
2017-02-10 11:29:12 -0500

    add optional theme argument to convert_less task

--- Gruntfile.js
+++ Gruntfile.js
@@ -203,9 +203,9 @@ grunt.registerTask('build_scss', 'build a regular theme from scss', function(the
   * Regex borrowed form
   * https://gist.github.com/rosskevin/ddfe895091de2ca5f931
   * */
-  grunt.registerTask('convert_less', 'Convert less to scss using regular expression', function () {
+  grunt.registerTask('convert_less', 'Convert less to scss using regular expression', function (theme) {
     var convertBaseDir = '';
-    grunt.file.expand(convertBaseDir + '*/*.less').forEach(function (lessFile) {
+    grunt.file.expand(convertBaseDir + (theme ? ('*' + theme) : '*') + '/*.less').forEach(function (lessFile) {
       if (lessFile !=="global/build.less"){
         var srcContents = grunt.file.read(lessFile);
         var out = srcContents
