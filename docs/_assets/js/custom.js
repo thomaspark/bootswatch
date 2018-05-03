77a8e45aa19a9a7c6e1d2c600d66e5b80589979a
Thomas Park <thomas@thomaspark.co>
2018-05-02 20:45:44 -0400

    refresh site styles

--- docs/_assets/js/custom.js
+++ docs/_assets/js/custom.js
@@ -1,9 +1,6 @@
 (function(){
   $(window).scroll(function () {
       var top = $(document).scrollTop();
-      $('.splash').css({
-        'background-position': '0px -'+(top/3).toFixed(2)+'px'
-      });
       if(top > 50)
         $('#home > .navbar').removeClass('navbar-transparent');
       else
