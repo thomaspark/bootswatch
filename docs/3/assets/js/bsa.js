bbf98f20a7bff7168ea0d120c73529f69677a77b
Thomas Park <hello@thomaspark.co>
2018-12-18 15:56:29 -0500

    v3.4.0

--- docs/3/assets/js/bsa.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  var bsa = document.createElement('script');
-     bsa.type = 'text/javascript';
-     bsa.async = true;
-     bsa.src = 'http://s3.buysellads.com/ac/bsa.js';
-  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
-})();
